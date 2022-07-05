ee2efd1416958033c7ad74d86ae14c2ad41ce3d3
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2022-07-05 16:11:58 +0200

    Formatting

--- backend/domain/add_path.go
+++ backend/domain/add_path.go
@@ -10,6 +10,12 @@ import (
 func (a API) AddPath(_ context.Context, input model.NewPath) (*model.Path, error) {
 	path := database.Path{Path: input.Path}
 	a.db.Create(&path)
-	dto := &model.Path{ID: int(path.ID), Path: path.Path, CreatedAt: path.CreatedAt.String()}
+
+	dto := &model.Path{
+		ID:        int(path.ID),
+		Path:      path.Path,
+		CreatedAt: path.CreatedAt.String(),
+	}
+
 	return dto, nil
 }
