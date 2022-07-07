c425b5bde338ac3e66e33bcfac2a9104a1812a70
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2022-07-07 16:26:18 +0200

    Able to get data through graphql api

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,15 @@
 import {Component} from '@angular/core';
+import {Apollo, gql} from "apollo-angular";
+import {map, Observable, of, tap} from "rxjs";
+import {Picture} from "./models";
+
+const GetPictures = gql`
+  query GetPictures {
+    GetPictures {
+      id, path
+    }
+  }
+`
 
 @Component({
   selector: 'app-root',
@@ -6,5 +17,20 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'laosyne';
+
+  loading = true;
+  error: any;
+  pictures$: Observable<Picture[]> = of([]);
+
+  constructor(private apollo: Apollo) {
+  }
+
+  ngOnInit() {
+    this.pictures$ = this.apollo.watchQuery<any>({query: GetPictures}).valueChanges.pipe(
+      tap(res => this.loading = res.loading),
+      tap(res => this.error = res.error),
+      map(res => res.data.GetPictures),
+      map(res => res as Picture[])
+    );
+  }
 }
