34472b88ff8220ada450c0629a6012e4b774344c
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2022-07-21 15:24:50 +0200

    Match styles improved

--- frontend/src/app/components/match/match.component.ts
+++ frontend/src/app/components/match/match.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { CreateMatchGQL, CreateMatchQuery, ReportMatchWinnerGQL } from "../../../generated/graphql";
-import { filter, map, Observable, of } from "rxjs";
+import { filter, map, Observable, of, tap } from "rxjs";
+
+export enum KEY_CODE {
+  RIGHT_ARROW = 'ArrowRight',
+  LEFT_ARROW = 'ArrowLeft'
+}
 
 @Component({
   selector: 'app-match',
@@ -9,6 +14,10 @@ import { filter, map, Observable, of } from "rxjs";
 })
 export class MatchComponent implements OnInit {
   public result$: Observable<CreateMatchQuery> = of({} as CreateMatchQuery);
+
+  // Only used by key listener func.
+  private playerTwoId: number = 0;
+  private playerOneId: number = 0;
 
   constructor(private match: CreateMatchGQL,
               private report: ReportMatchWinnerGQL) {
@@ -21,12 +30,24 @@ export class MatchComponent implements OnInit {
   private createMatch() {
     this.result$ = this.match.watch().valueChanges.pipe(
       filter(res => !!res.data.Match.playerOne.id),
-      map(res => res.data));
+      map(res => res.data),
+      tap(data => this.playerOneId = data.Match.playerOne.id),
+      tap(data => this.playerTwoId = data.Match.playerTwo.id),
+    );
   }
 
   public reportResult(winnerId: number, loserId: number): void {
     this.report.mutate({winnerId: winnerId, loserId: loserId}).subscribe();
     this.match.watch().refetch();
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent): void {
+    if (event.key === KEY_CODE.RIGHT_ARROW || event.key === 'd') {
+      this.reportResult(this.playerTwoId, this.playerOneId);
+    } else if (event.key === KEY_CODE.LEFT_ARROW || event.key === 'a') {
+      this.reportResult(this.playerOneId, this.playerTwoId);
+    }
   }
 
 }
